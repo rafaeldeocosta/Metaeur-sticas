207df3ed7fbd6da80b595c0f4e4b33213248f5e9
Rafael Costa <rafaeldeoliveiracosta@gmail.com>
2022-01-20 13:03:35 -0300

    parametros da função metropolis alterado

--- metropolis.py
+++ metropolis.py
@@ -1,13 +1,15 @@
-def metropolis(S_ini, Temperature):
+def metropolis(S_ini, Temperature, n_iter_temperature):
     """"
         função que implementa o algoritmo de metropolis
 
         Args:
-            S_ini - igraph.Graph - Solution of PCSTP ("Steiner" Tree) 
+            S_ini - igraph.Graph - initial Solution of PCSTP ("Steiner" Tree)
             Temperature - float - Temperature used to calculate probability to
                 accept worst solutions
+            n_iter_temperature - maximum of iteration in this Temperature
 
         Returns:
+            S_ - igraph.Graph - Solution of PCSTP ("Steiner" Tree)
 
     """"
 
