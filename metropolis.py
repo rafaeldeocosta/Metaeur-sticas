39d234781ac8e73851b7a9bedb3d71965e64da7b
Rafael Costa <rafaeldeoliveiracosta@gmail.com>
2022-01-20 12:47:14 -0300

    metropolis inicial

---
+++ metropolis.py
@@ -0,0 +1,14 @@
+def metropolis(S_ini, Temperature):
+    """"
+        função que implementa o algoritmo de metropolis
+
+        Args:
+            S_ini - igraph.Graph - Solution of PCSTP ("Steiner" Tree) 
+            Temperature - float - Temperature used to calculate probability to
+                accept worst solutions
+
+        Returns:
+
+    """"
+
+    return S_
